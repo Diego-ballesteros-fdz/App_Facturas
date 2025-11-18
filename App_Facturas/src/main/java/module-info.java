d24eaac792320f410de7308a25c9a5e3a4c25d34
Diego Ballesteros <dtbf91@educa.madrid.org>
2025-11-18 12:44:47 +0100

    añade formulary.fxml

--- App_Facturas/src/main/java/module-info.java
+++ App_Facturas/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.app_facturas.app_facturas {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.j;
+    
 
     opens com.app_facturas.app_facturas to javafx.fxml;
     exports com.app_facturas.app_facturas;
